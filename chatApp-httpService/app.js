495b72e1a46a696cf2b7c8d8b6d19269d6e9ea45
Samantha Spensley <spenslsm@gmail.com>
2016-10-18 21:11:05 -0600

    chat app working

---
+++ chatApp-httpService/app.js
@@ -0,0 +1,23 @@
+var app = angular.module('chatApp', []);
+
+app.controller('MainController', function($scope, $http){
+  $scope.view = {};
+
+  $http.get('https://messagehttpservice.herokuapp.com/messages')
+  .success(function(data){
+    $scope.view.messages = data;
+  })
+  .catch(function(error){
+    $scope.view.messages = error.status;
+  });
+
+  $scope.submitQuote = function(){
+    var data = {
+      message: {
+        name: $scope.view.name,
+        content: $scope.view.quote
+      }
+    };
+    $http.post('https://messagehttpservice.herokuapp.com/messages', data);
+  };
+});
